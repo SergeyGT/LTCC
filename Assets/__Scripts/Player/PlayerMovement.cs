fef1b9422cc6e237da21bfc34d69f943c1dd7704
SergeyGT <sergeyburdenyuk@mail.ru>
2026-04-08 23:21:19 +0300

    Upd: Добавлен прыжок, настроены параметры передвижения

--- Assets/__Scripts/Player/PlayerMovement.cs
+++ Assets/__Scripts/Player/PlayerMovement.cs
@@ -10,12 +10,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _currentSpeed;
     [SerializeField] private float _acceleration;
     [SerializeField] private float _gravityForce;
+    [SerializeField] private float _jumpForce;
     
     private CharacterController _controller;
     private Vector3 _moveDirection;
     private PlayerInput _playerInput;
     private float _targetSpeed;
-
+    private float _verticalVelocity;
+    
     private void Awake()
     {
         _playerInput = new PlayerInput();
@@ -30,12 +32,17 @@ public class PlayerMovement : MonoBehaviour
     {
         _playerInput.Player.Sprint.performed += StartSprint;
         _playerInput.Player.Sprint.canceled += StopSprint;
+
+        _playerInput.Player.Jump.performed += Jump;
     }
 
     private void OnDisable()
     {
         _playerInput.Player.Sprint.performed -= StartSprint;
         _playerInput.Player.Sprint.canceled -= StopSprint;
+        
+        _playerInput.Player.Jump.performed -= Jump;
+        
         _playerInput.Disable();
     }
 
@@ -48,16 +55,21 @@ public class PlayerMovement : MonoBehaviour
     {
         _targetSpeed = _speedWalk;
     }
-    
-    private void Update()
+
+    private void Jump(InputAction.CallbackContext ctx)
     {
-        ReadMovement();
-    }
+       if(_controller.isGrounded) _verticalVelocity = _jumpForce;
+    } 
+    
+    private void Update() => ReadMovement();
     
     private void ReadMovement()
     {
         var directionInput = _playerInput.Player.Move.ReadValue<Vector2>();
-        Vector3 moveDirection = new Vector3(directionInput.x, _gravityForce, directionInput.y);
+
+        _verticalVelocity += _gravityForce * Time.fixedDeltaTime;
+        
+        Vector3 moveDirection = new Vector3(directionInput.x, _verticalVelocity, directionInput.y);
         
         if (_cameraRotate != null)
             _moveDirection = Quaternion.Euler(0, _cameraRotate.eulerAngles.y, 0) * moveDirection;
@@ -67,18 +79,14 @@ public class PlayerMovement : MonoBehaviour
     
     private void UpdateSpeed()
     {
+        if (!IsMoving()) return;
+        
         _currentSpeed = Mathf.Lerp(_currentSpeed, _targetSpeed, _acceleration * Time.fixedDeltaTime);
         
         if (Mathf.Abs(_currentSpeed - _targetSpeed) < 0.01f)
             _currentSpeed = _targetSpeed;
     }
     
-    private void FixedUpdate()
-    {
-        UpdateSpeed();
-        Move();
-        Rotate();
-    }
 
     private void Rotate()
     {
@@ -91,5 +99,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = _moveDirection * _currentSpeed * Time.fixedDeltaTime;
         
         _controller.Move(move);
+    }
+
+    private bool IsMoving()
+    {
+        var move = _playerInput.Player.Move.ReadValue<Vector2>();
+        return move.magnitude > 0.2f;
+    }
+    private void FixedUpdate()
+    {
+        UpdateSpeed();
+        Move();
+        Rotate();
     }
 }
