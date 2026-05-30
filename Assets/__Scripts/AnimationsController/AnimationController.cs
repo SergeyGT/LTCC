43f0b87972f041021ce76842557343dc7b307b5d
SergeyGT <sergeyburdenyuk@mail.ru>
2026-05-31 00:55:23 +0300

    Upd: написан класс для звуковых эффектов при анимации

--- Assets/__Scripts/AnimationsController/AnimationController.cs
+++ Assets/__Scripts/AnimationsController/AnimationController.cs
@@ -1,24 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using __Scripts.System;
+using UnityEngine;
 using EventReference = FMODUnity.EventReference;
+using Random = System.Random;
 
 namespace __Scripts.AnimationsController
 {
-    [Serializable]
-    public class AnimationController 
+    public class AnimationController : MonoBehaviour
     {
-        public SerializableDictionary<TypeSound, EventReference> audioClips;
+        public List<Sound> audioClips;
 
-        public void OneFoot()
+        public void Step(string clipName)
         {
-            //EventReference eventReference = audioClips[TypeSound.Walk];
+            foreach (Sound clip in audioClips)
+            {
+                if (clip.nameOfSound == clipName)
+                {
+                    Random rnd = new Random();
+                    int indexClip = rnd.Next(clip.clips.Count);
+                    AudioManager.Instance.PlayOneShot(clip.clips[indexClip]);
+                }
+            }
         }
     }
 
     [Serializable]
-    public enum TypeSound
+    public class Sound
+    {
+        public string nameOfSound;
+        public List<EventReference> clips;
+        public Surface surface;
+        public SoundType type;
+        [Range(0,2)] public float pitch;
+    }
+
+    [Serializable]
+    public enum Surface
+    {
+        Wood,
+        Metal,
+        Water,
+        Tile
+    }
+
+    [Serializable]
+    public enum SoundType
     {
         Walk,
-        Interact
+        Jump,
+        Interact,
+        Use
     }
 }
